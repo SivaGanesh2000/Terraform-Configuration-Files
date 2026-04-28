723a297847d73a213be36d1fd2900dd0d120f6b7
SivaGanesh2000 <learnfr0my0utube@gmail.com>
2026-04-28 15:14:59 +0530

    Update IAM_policy.tf

--- IAM_Role/IAM_policy.tf
+++ IAM_Role/IAM_policy.tf
@@ -4,17 +4,18 @@ resource "aws_iam_role_policy" "pol"{
     name = var.inline_policy_name
     role = aws_iam_role.role.id
 
-    policy = try(file(var.inline-policy), null)
+    policy = try(jsonencode(yamldecode(file(var.inline-policy))), null)
 }
 
 resource "aws_iam_policy" "policy" {
   count = var.manag-policy != null ? 1:0
   name = var.manag_policy_name
-  policy = try(file(var.manag-policy), null)
+  policy = try(jsonencode(yamldecode(file(var.manag-policy))), null)
 }
 
 
 resource "aws_iam_role_policy_attachment" "rpa" {
+    count = var.manag-policy != null ? 1:0
     role = aws_iam_role.role.name
-    policy_arn = aws_iam_policy.policy.arn 
+    policy_arn = aws_iam_policy.policy[*].arn 
 }
