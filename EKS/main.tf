bf061f20753daf3dbac085d08ae363264b9947f0
SivaGanesh2000 <learnfr0my0utube@gmail.com>
2026-04-30 13:07:10 +0530

    Need to test changes

--- EKS/main.tf
+++ EKS/main.tf
@@ -2,10 +2,15 @@ resource "aws_eks_cluster" "eks"{
     name = var.cluster_Name
     version = var.cluster_version
 
+    access_config {
+      authentication_mode = "API_AND_CONFIG_MAP"
+      bootstrap_cluster_creator_admin_permissions = true
+    }
 
     role_arn = module.iam_role.IAM_Details["Role_Arn"]
 
     vpc_config {
+      endpoint_public_access = true
 
       subnet_ids = [for name in module.vpc.Public_Subnet_Details: name.Id]
     }
